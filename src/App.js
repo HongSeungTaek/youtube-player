2480607f411bfd4d6cfa4331f749fd1399565313
홍승택 <tmdxor66@naver.com>
2021-07-20 00:11:39 +0900

    키워드 검색 구현

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import YTsearch from 'youtube-api-search';
+import { Grid } from '@material-ui/core';
+
 import './App.css';
 
-const API_KEY = '';
+const API_KEY = 'AIzaSyCUnxBF0hQKytax-8Ni15aKl-EvlJsrxsM';
 
 function App() {
   let [videos, setVideos] = React.useState([]);
+  let [keyword, setKeyword] = React.useState('');
+
   function searchYoutube(term) {
-    YTsearch({key: API_KEY, term: 'blackpink'}, (res) => {
+    YTsearch({key: API_KEY, term: keyword}, (res) => {
       console.log(res);
       setVideos(res);
       console.log(videos);
@@ -16,12 +20,23 @@ function App() {
   return (
     <div className="App">
       <div>
+        <input type="text" value={keyword} onChange={(e)=>{ setKeyword(e.target.value) }}></input>
         <input type="button" onClick={searchYoutube} value="Click" ></input>
       </div>
 
-
+      
+      <Grid container spacing={1}>
       {/* {videos.map((item) => <img src={item.snippet.thumbnails.medium.url} width="320" height="180"/>)} */}
-      {videos.map((item) => <iframe key={item.id.videoId} className="embed-responsive-item" src={'https://www.youtube.com/embed/'+item.id.videoId} width="320" height="180"></iframe>)}
+      {videos.map((item) =>
+      <>
+        <Grid item xs>
+          <iframe key={item.id.videoId} className="embed-responsive-item"
+                  src={'https://www.youtube.com/embed/'+item.id.videoId}></iframe>
+          <div>{item.snippet.title}</div>
+        </Grid>
+      </>
+      )}
+      </Grid>
     </div>
   );
 }
